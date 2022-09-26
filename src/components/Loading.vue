97991b5a5817705a638f2371df23068320160662
canberk-yilmaz <canberk.yilmaz@reengen.com>
2022-09-26 18:08:54 +0300

    loading component add

---
+++ src/components/Loading.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex items-center justify-center space-x-2 animate-pulse">
+    <div class="w-8 h-8 bg-green-400 rounded-full"></div>
+    <div class="w-8 h-8 bg-green-400 rounded-full"></div>
+    <div class="w-8 h-8 bg-green-400 rounded-full"></div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Loading",
+};
+</script>
