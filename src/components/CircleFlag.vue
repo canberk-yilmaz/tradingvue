8c7dad8e36f0c11d4f5d52fdd2cabaece0637cfb
canberk-yilmaz <canberk.yilmaz@reengen.com>
2022-09-26 23:58:48 +0300

    flag component create

---
+++ src/components/CircleFlag.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    :class="`rounded-full bg-center object-center currency-flag currency-flag-${currency.toLowerCase()} mr-2`"
+    style="height: 24px; width: 24px"
+  ></div>
+</template>
+<script>
+export default {
+  props: {
+    currency: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
