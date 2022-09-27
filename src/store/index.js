e87a5fd3b856a162bd37351c8a2c34f0ee9e441a
canberk-yilmaz <cyilmaz089@gmail.com>
2022-09-27 16:53:36 +0300

    func move to store from component

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,29 @@
 import Vue from "vue";
 import Vuex from "vuex";
-
+import axios from "axios";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    currenciesList: null,
+  },
+  mutations: {
+    SET_CURRENCIES_LIST(state, currenciesList) {
+      state.currenciesList = currenciesList;
+    },
+  },
+  actions: {
+    async getCurrenciesList({ commit }) {
+      try {
+        const res = await axios.get(
+          `https://marketdata.tradermade.com/api/v1/live_currencies_list?api_key=${process.env.VUE_APP_REST_API_KEY}`
+        );
+        console.log("res store", res);
+        commit("SET_CURRENCIES_LIST", res.data.available_currencies);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
   modules: {},
 });
