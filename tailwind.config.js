3a3a1488c70888df145cdfdcd4b859ef5aa5864d
canberk-yilmaz <cyilmaz089@gmail.com>
2022-09-28 16:18:25 +0300

    config update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-
+  purge: [
+    "./views/**/*.vue",
+    "./components/**/*.vue",
+    "./plugins/**/*.vue",
+    "./static/**/*.vue",
+    "./store/**/*.vue",
+  ],
   theme: {
     extend: {},
   },
